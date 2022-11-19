47c03ea7d554ad2ea8e04f4707e17cf301095618
loksantosh <gloksantosh4@gmail.com>
2022-11-19 10:38:17 +0530

    PassportJs

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const app = express()
-const bcrypt=require('bcrypt')
-const mongoose=require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
 
+const mongoose = require('mongoose')
 
-
-const userModel=require(__dirname+"/models/userModel")
+const userModel = require(__dirname + "/models/userModel")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -15,7 +15,22 @@ app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-mongoose.connect(process.env.DATA_BASE,{ useNewUrlParser: true }).then(() => console.log("Database connected"), (error) => console.log(error))
+app.use(session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(userModel.createStrategy());
+
+passport.serializeUser(userModel.serializeUser());
+passport.deserializeUser(userModel.deserializeUser());
+
+
+mongoose.connect(process.env.DATA_BASE, { useNewUrlParser: true }).then(() => console.log("Database connected"), (error) => console.log(error))
 
 
 app.get('/', (req, res) => {
@@ -30,21 +45,50 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.post('/register' ,async(req,res)=>{
-    const {username ,password}=req.body
-    const salt= await bcrypt.genSalt(10)
-    console.log(salt)
-    const hashPassword =await bcrypt.hash(password,salt)
-   await userModel.create({username:username ,password:hashPassword})
-   res.render('secrets')
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated())
+        res.render('secrets')
+
+    else res.redirect('/login')
 })
 
-app.post('/login' ,async(req,res)=>{
-    let {username ,password}=req.body
-    const check=await userModel.findOne({username:username})
-    const checkPass=  await bcrypt.compare(password ,check.password)
-    if(checkPass)
-   console.log("verified" ,check)
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body
+    const registerUser = await userModel.register({ username: username, active: false }, password)
+    if (!registerUser)
+        res.redirect('/register')
+
+    passport.authenticate('local')(req, res, () => {
+        res.redirect('/secrets')
+
+    })
+
+});
+
+app.get('/logout', (req, res) => {
+    req.logout((error) => {
+        if (error) console.log(error)
+        res.redirect('/')
+    })
+
+})
+
+app.post('/login', async (req, res) => {
+    let { username, password } = req.body
+    const user = {
+        username: username,
+        password: password
+    }
+    req.login(user, (error) => {
+        if (error) {
+            console.log(error)
+            res.redirect('/login')
+        }
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/secrets')
+        })
+    })
+
     res.render('secrets')
 })
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,16 @@
 const mongoose=require('mongoose')
+const passportLocalMongoose=require('passport-local-mongoose')
+
 const userModel=new mongoose.Schema({
     username:{
         type:String,
         required:true
     },
     password:{
-       type:String,
-       required:true
-    }
+       type:String
+      }
 })
+
+userModel.plugin(passportLocalMongoose)
+
 module.exports=mongoose.model('user',userModel)
